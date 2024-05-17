b6995f31f2fb6da9fffbf4a97eb91c231559948a
hongcheol <hongcheol6689@gmail.com>
2024-05-17 12:46:33 +0900

    갤러리 페이지 : 사진 선택하면 set에 담기, 다시 선택하면 삭제하기 / 여기서 stack으로 티나게 만들고, 이 set를 falsk로 보내서 aws에서 지우기만 하면 끝git add .

--- room_mate_flutter/lib/view/gallery.dart
+++ room_mate_flutter/lib/view/gallery.dart
@@ -12,21 +12,27 @@ class Gallery extends StatefulWidget {
 
 class _GalleryState extends State<Gallery> {
   late Future<List<String>> _imageUrls;
-  bool selected = false;
-  List selectedList = [];
+  List selected = []; // boolean List : 선택 됐는지 안됐는지 확인
+  Set selectedList = {}; // 선택된 사진들 담는 set
+  bool visibleButton = false; // 사진 선택 전에 버튼 안보이도록
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _imageUrls = getImageUrls();
+
+    // initState에서는 async-await 못쓰니 .then 사용하면 된다!!
+    _imageUrls.then((value) {
+      int length = value.length;
+      print("List 길이 :" + length.toString());
+      selected = List.filled(length, false);
+      print(selected);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // aws에서 불러온 사진리스트
-    List<String> photoList = [];
-
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50.0),
@@ -52,32 +58,42 @@ class _GalleryState extends State<Gallery> {
               future: _imageUrls,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return SingleChildScrollView(
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 8.0,
-                        mainAxisSpacing: 8.0,
-                      ),
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              selected = true;
-                            });
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.network(
-                              snapshot.data![index],
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        );
-                      },
+                  return GridView.builder(
+                    // shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 8.0,
+                      mainAxisSpacing: 8.0,
                     ),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          selected[index] = !selected[index];
+                          if (selected[index]) {
+                            selectedList.add(snapshot.data![index]);
+                          } else {
+                            selectedList.remove(snapshot.data![index]);
+                          }
+
+                          if (selectedList.length != 0) {
+                            visibleButton = true;
+                          } else {
+                            visibleButton = false;
+                          }
+                          print(selected);
+                          print("리스트들!!" + selectedList.toString());
+                          setState(() {});
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.network(
+                            snapshot.data![index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
                   );
                 } else if (snapshot.hasError) {
                   return Center(
@@ -92,7 +108,7 @@ class _GalleryState extends State<Gallery> {
             ),
           ),
           Visibility(
-            visible: selected,
+            visible: visibleButton,
             child: ElevatedButton(
               onPressed: () {},
               child: Text('선택 사진 삭제'),
@@ -109,7 +125,7 @@ Future<List<String>> getImageUrls() async {
     final response = await Dio().get('http://121.147.52.9:8016/get_photos');
     if (response.statusCode == 200) {
       final imageUrls = List<String>.from(response.data);
-      print("사진들: " + response.data.toString());
+      print("사진들: " + imageUrls.toString());
       return imageUrls;
     } else {
       throw Exception('Failed to fetch image URLs');
